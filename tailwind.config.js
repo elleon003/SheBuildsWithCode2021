99bc59ffb41c3d8261e8b7c639dfdde5f5bdc94c
elleon003 <ngrandison@ymail.com>
2021-02-22 18:44:50 -0500

    Version 1 of homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,52 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Heebo', 'ui-sans-serif', 'system-ui', 'sans-serif']
+    },
+    colors: {
+      black: {
+        DEFAULT: '#080708'
+      },
+      white: {
+        DEFAULT: '#F5F5F5'
+      },
+      grey: {
+        DEFAULT: '#707070'
+      },
+      red: {
+        light: '#E25C6E',
+        DEFAULT: '#D7263D',
+        dark: '#A31D2F'
+      },
+      purple: {
+        light: '#9040D1',
+        DEFAULT: '#68269D',
+        dark: '#4D1C75'
+      },
+      pink: {
+        DEFAULT: '#F1DEDC',
+        dark: '#D09088'
+      }
+    },
+    container: {
+      center: true,
+    },
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      fontWeight: ['hover'],
+      fontStyle: ['hover'],
+      display: ['dark'],
+      borderWidth: ['active', 'focus'],
+      borderColor: ['active', 'focus']
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
